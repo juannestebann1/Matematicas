2c38aed2182ace8b9298fad7154623fb31681c10
juannestebann1 <juannestebann1@gmail.com>
2017-06-26 01:37:16 -0500

    configure
    agregando constant's

--- Configure.py
+++ Configure.py
@@ -1,1 +1,4 @@
 # Script de configuracion "aqui estan todas la variables globales"
+
+AUTORES = "Juan esteban alvarez & Julian Camilo Rodriguez"
+VERSION = 0.1
--- Main.py
+++ Main.py
@@ -1,3 +1,10 @@
 import Configure as cf
 
 #Archivo donde se creara toda las funciones de el modulo de matematicas
+
+print(cf.AUTORES)
+
+if cf.VERSION > 1.0:
+	print("la version es la mas actualizada")
+else:
+	print("Debes actualizar de version a la " + str(cf.VERSION))
